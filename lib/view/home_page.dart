ee412d13a98e7a5dc639fef57ce25fccd55205d3
GabrielMSchmidt <gabrielmarochischmidt@gmail.com>
2024-11-18 20:26:15 -0300

    First Commit Gabriel

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -45,7 +45,7 @@ class _HomePageState extends State<HomePage> {
               //   Navigator.push(context, MaterialPageRoute(builder: (context) => const PorExtensoPage()));
               // },
             ),
-            SizedBox(height: 30),
+            const SizedBox(height: 30),
             GestureDetector(
               child: const Row(
                 children: <Widget>[
